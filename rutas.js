4eb78826bbb8e977b8a5c8cd0c11fb265aff3cfb
batres <hbatress@miumg.edu.gt>
2023-10-18 01:49:25 -0600

    creación de la pagina principal con estilo

--- rutas.js
+++ rutas.js
@@ -2,7 +2,7 @@ module.exports = function(app, dbservice) {
 
     app.get('/Tipo_de_cuenta', (req, res) => {
         dbservice.gettipo_de_cuenta()
-            .then(lenguajes => res.json(lenguajes))
+            .then(Tipocuenta => res.json(Tipocuenta))
             .catch(e => res.status(500).send(e));
     });
 };
