7d309f6a352ce948968071e5de357a00f20666f0
batres <hbatress@miumg.edu.gt>
2023-10-20 00:07:42 -0600

    se cargo los post

--- Codigo de Base de datos/dbservice.js
+++ Codigo de Base de datos/dbservice.js
@@ -1,82 +1,245 @@
 const dbservice = () => {
-    const knex = require('knex')({
-        client: 'mysql',
+    const knex = require("knex")({
+        client: "mysql",
         connection: {
-            host : process.env.DB_HOST,
-            port : 3306,
-            user : process.env.DB_USER,
-            password : process.env.DB_PASS,
-            database : process.env.DB,
-        }
+            host: process.env.DB_HOST,
+            port: 3306,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASS,
+            database: process.env.DB,
+        },
     });
-// Declaraciones de constantes
-const Usuario = 'Usuario';
-const Tablero = 'Tablero';
-const ListaDeTarea = 'Lista_de_Tarea';
-const Tarjeta = 'Tarjeta';
-const Comentario = 'Comentario';
-const Etiqueta = 'Etiqueta';
-const HistorialDeMovimiento = 'Historial_de_Movimiento';
-const TipoDeMovimiento = 'tipo_de_movimiento';
-const TipoDeCuenta = 'tipo_de_cuenta';
-const TipoDePago = 'tipo_de_Pago';
-const EstadoDeLaTarjeta = 'Estado_de_la_Tarjeta';
-const Participante = 'Participante';
-const DatosDeTarjeta = 'Datos_de_Tarjeta';
+    // Declaraciones de constantes
+    const Usuario = "Usuario";
+    const Tablero = "Tablero";
+    const ListaDeTarea = "Lista_de_Tarea";
+    const Tarjeta = "Tarjeta";
+    const Comentario = "Comentario";
+    const Etiqueta = "Etiqueta";
+    const HistorialDeMovimiento = "Historial_de_Movimiento";
+    const TipoDeMovimiento = "tipo_de_movimiento";
+    const TipoDeCuenta = "tipo_de_cuenta";
+    const TipoDePago = "tipo_de_Pago";
+    const EstadoDeLaTarjeta = "Estado_de_la_Tarjeta";
+    const Participante = "Participante";
+    const DatosDeTarjeta = "Datos_de_Tarjeta";
 
-// Funciones de consulta
-const getUsuario = () => {
-return knex(Usuario).select();
-};
+    // Funciones de consulta
+    const getUsuario = () => {
+        return knex(Usuario).select();
+    };
 
-const getTablero = () => {
-return knex(Tablero).select();
-};
+    const getTablero = () => {
+        return knex(Tablero).select();
+    };
 
-const getListaDeTarea = () => {
-return knex(ListaDeTarea).select();
-};
+    const getListaDeTarea = () => {
+        return knex(ListaDeTarea).select();
+    };
 
-const getTarjeta = () => {
-return knex(Tarjeta).select();
-};
+    const getTarjeta = () => {
+        return knex(Tarjeta).select();
+    };
 
-const getComentario = () => {
-return knex(Comentario).select();
-};
+    const getComentario = () => {
+        return knex(Comentario).select();
+    };
 
-const getEtiqueta = () => {
-return knex(Etiqueta).select();
-};
+    const getEtiqueta = () => {
+        return knex(Etiqueta).select();
+    };
 
-const getHistorialDeMovimiento = () => {
-return knex(HistorialDeMovimiento).select();
-};
+    const getHistorialDeMovimiento = () => {
+        return knex(HistorialDeMovimiento).select();
+    };
 
-const getTipoDeMovimiento = () => {
-return knex(TipoDeMovimiento).select();
-};
+    const getTipoDeMovimiento = () => {
+        return knex(TipoDeMovimiento).select();
+    };
 
-const getTipoDeCuenta = () => {
-return knex(TipoDeCuenta).select();
-};
+    const getTipoDeCuenta = () => {
+        return knex(TipoDeCuenta).select();
+    };
 
-const getTipoDePago = () => {
-return knex(TipoDePago).select();
-};
+    const getTipoDePago = () => {
+        return knex(TipoDePago).select();
+    };
 
-const getEstadoDeLaTarjeta = () => {
-return knex(EstadoDeLaTarjeta).select();
-};
+    const getEstadoDeLaTarjeta = () => {
+        return knex(EstadoDeLaTarjeta).select();
+    };
 
-const getParticipante = () => {
-return knex(Participante).select();
-};
+    const getParticipante = () => {
+        return knex(Participante).select();
+    };
 
-const getDatosDeTarjeta = () => {
-return knex(DatosDeTarjeta).select();
-};
-    return {getUsuario,
+    const getDatosDeTarjeta = () => {
+        return knex(DatosDeTarjeta).select();
+    };
+
+    //Funciones para ingresar
+    const crearUsuario = (
+        Nombre,
+        Correo_Electronico,
+        Rol,
+        Fecha_de_Registro,
+        Tipo_de_Cuenta,
+        Tipo_de_Pago
+    ) => {
+        return knex(Usuario).insert({
+            Nombre: Nombre,
+            Correo_Electronico: Correo_Electronico,
+            Rol: Rol,
+            Fecha_de_Registro: Fecha_de_Registro,
+            Tipo_de_Cuenta: Tipo_de_Cuenta,
+            Tipo_de_Pago: Tipo_de_Pago,
+        });
+    };
+
+    const crearTablero = (
+        Nombre_del_Tablero,
+        Descripcion,
+        Fecha_de_Creacion,
+        Usuario_Propietario
+    ) => {
+        return knex(Tablero).insert({
+            Nombre_del_Tablero: Nombre_del_Tablero,
+            Descripcion: Descripcion,
+            Fecha_de_Creacion: Fecha_de_Creacion,
+            Usuario_Propietario: Usuario_Propietario,
+        });
+    };
+
+    const crearListaDeTarea = (
+        Nombre_de_la_Lista,
+        Posicion_en_el_Tablero,
+        Tablero_Perteneciente
+    ) => {
+        return knex(ListaDeTarea).insert({
+            Nombre_de_la_Lista: Nombre_de_la_Lista,
+            Posicion_en_el_Tablero: Posicion_en_el_Tablero,
+            Tablero_Perteneciente: Tablero_Perteneciente,
+        });
+    };
+
+    const crearTarjeta = (
+        Titulo_de_la_Tarjeta,
+        Descripcion,
+        Fecha_de_Creacion,
+        Lista_Perteneciente,
+        Asignado_a,
+        Estado_de_la_Tarjeta
+    ) => {
+        return knex(Tarjeta).insert({
+            Titulo_de_la_Tarjeta: Titulo_de_la_Tarjeta,
+            Descripcion: Descripcion,
+            Fecha_de_Creacion: Fecha_de_Creacion,
+            Lista_Perteneciente: Lista_Perteneciente,
+            Asignado_a: Asignado_a,
+            Estado_de_la_Tarjeta: Estado_de_la_Tarjeta,
+        });
+    };
+
+    const crearComentario = (
+        Texto_del_Comentario,
+        Fecha_de_Publicacion,
+        Usuario_que_Realizo_el_Comentario,
+        Tarjeta_que_se_Refiere
+    ) => {
+        return knex(Comentario).insert({
+            Texto_del_Comentario: Texto_del_Comentario,
+            Fecha_de_Publicacion: Fecha_de_Publicacion,
+            Usuario_que_Realizo_el_Comentario: Usuario_que_Realizo_el_Comentario,
+            Tarjeta_que_se_Refiere: Tarjeta_que_se_Refiere,
+        });
+    };
+
+    const crearEtiqueta = (Nombre_de_la_Etiqueta, Color_de_la_Etiqueta) => {
+        return knex(Etiqueta).insert({
+            Nombre_de_la_Etiqueta: Nombre_de_la_Etiqueta,
+            Color_de_la_Etiqueta: Color_de_la_Etiqueta,
+        });
+    };
+
+    const crearHistorialDeMovimiento = (
+        Fecha_y_Hora_del_Movimiento,
+        Usuario_que_Realizo_el_Movimiento,
+        Tipo_de_Movimiento,
+        Tarjeta_Afectada
+    ) => {
+        return knex(HistorialDeMovimiento).insert({
+            Fecha_y_Hora_del_Movimiento: Fecha_y_Hora_del_Movimiento,
+            Usuario_que_Realizo_el_Movimiento: Usuario_que_Realizo_el_Movimiento,
+            Tipo_de_Movimiento: Tipo_de_Movimiento,
+            Tarjeta_Afectada: Tarjeta_Afectada,
+        });
+    };
+
+    const crearTipoDeMovimiento = (
+        Nombre_del_Tipo_de_Movimiento,
+        Descripcion_del_Tipo_de_Movimiento
+    ) => {
+        return knex(TipoDeMovimiento).insert({
+            Nombre_del_Tipo_de_Movimiento: Nombre_del_Tipo_de_Movimiento,
+            Descripcion_del_Tipo_de_Movimiento: Descripcion_del_Tipo_de_Movimiento,
+        });
+    };
+
+    const crearTipoDeCuenta = (
+        Nombre_del_Tipo_de_Cuenta,
+        Descripcion_del_Tipo_de_Cuenta
+    ) => {
+        return knex(TipoDeCuenta).insert({
+            Nombre_del_Tipo_de_Cuenta: Nombre_del_Tipo_de_Cuenta,
+            Descripcion_del_Tipo_de_Cuenta: Descripcion_del_Tipo_de_Cuenta,
+        });
+    };
+
+    const crearTipoDePago = (
+        Nombre_del_Tipo_de_Pago,
+        Descripcion_del_Tipo_de_Pago
+    ) => {
+        return knex(TipoDePago).insert({
+            Nombre_del_Tipo_de_Pago: Nombre_del_Tipo_de_Pago,
+            Descripcion_del_Tipo_de_Pago: Descripcion_del_Tipo_de_Pago,
+        });
+    };EstadoDeLaTarjeta
+
+    const crearEstadoDeLaTarjeta = (
+        Nombre_del_Estado,
+        Descripcion_del_Estado
+    ) => {
+        return knex(EstadoDeLaTarjeta).insert({
+            Nombre_del_Estado: Nombre_del_Estado,
+            Descripcion_del_Estado: Descripcion_del_Estado,
+        });
+    };
+
+    const crearParticipante = (Usuario_Participante, Tarjeta_asociada) => {
+        return knex(Participante).insert({
+            Usuario_Participante: Usuario_Participante,
+            Tarjeta_asociada: Tarjeta_asociada,
+        });
+    };
+
+    const crearDatosDeTarjeta = (
+        Numero_de_Tarjeta,
+        Mes_de_Vencimiento,
+        Ano_de_Vencimiento,
+        Codigo_de_Seguridad,
+        Tipo_de_Pago
+    ) => {
+        return knex(DatosDeTarjeta).insert({
+            Numero_de_Tarjeta: Numero_de_Tarjeta,
+            Mes_de_Vencimiento: Mes_de_Vencimiento,
+            Ano_de_Vencimiento: Ano_de_Vencimiento,
+            Codigo_de_Seguridad: Codigo_de_Seguridad,
+            Tipo_de_Pago: Tipo_de_Pago,
+        });
+    };
+
+    return {
+        getUsuario,
         getTablero,
         getListaDeTarea,
         getTarjeta,
@@ -89,11 +252,23 @@ return knex(DatosDeTarjeta).select();
         getEstadoDeLaTarjeta,
         getParticipante,
         getDatosDeTarjeta,
-    };
+        crearUsuario,
+        crearTablero,
+        crearListaDeTarea,
+        crearTarjeta,
+        crearComentario,
+        crearEtiqueta,
+        crearHistorialDeMovimiento,
+        crearTipoDeMovimiento,
+        crearTipoDeCuenta,
+        crearTipoDePago,
+        crearEstadoDeLaTarjeta,
+        crearParticipante,
+        crearDatosDeTarjeta
 
+    };
 };
 
 module.exports = {
-    dbservice
+    dbservice,
 };
-
--- rutas.js
+++ rutas.js
@@ -1,85 +1,313 @@
-module.exports = function(app, dbservice) {
+module.exports = function (app, dbservice) {
     app.get('/', (req, res) => {
 
-        res.json({"Mensaje":"Todo bien"});
+        res.json({ "Mensaje": "Todo bien" });
     });
 
     //los get para leer la informacion 
     app.get('/Usuario', (req, res) => {
         dbservice.getUsuario()
-        .then(usuarios => res.json(usuarios))
-        .catch(error => res.status(500).send(error));
+            .then(usuarios => res.json(usuarios))
+            .catch(error => res.status(500).send(error));
     });
-    
+
     app.get('/Tablero', (req, res) => {
         dbservice.getTablero()
-        .then(tableros => res.json(tableros))
-        .catch(error => res.status(500).send(error));
+            .then(tableros => res.json(tableros))
+            .catch(error => res.status(500).send(error));
     });
-    
+
     app.get('/Lista_de_Tarea', (req, res) => {
         dbservice.getListaDeTarea()
-        .then(listas => res.json(listas))
-        .catch(error => res.status(500).send(error));
+            .then(listas => res.json(listas))
+            .catch(error => res.status(500).send(error));
     });
-    
+
     app.get('/Tarjeta', (req, res) => {
         dbservice.getTarjeta()
-        .then(tarjetas => res.json(tarjetas))
-        .catch(error => res.status(500).send(error));
+            .then(tarjetas => res.json(tarjetas))
+            .catch(error => res.status(500).send(error));
     });
-    
+
     app.get('/Comentario', (req, res) => {
         dbservice.getComentario()
-        .then(comentarios => res.json(comentarios))
-        .catch(error => res.status(500).send(error));
+            .then(comentarios => res.json(comentarios))
+            .catch(error => res.status(500).send(error));
     });
-    
+
     app.get('/Etiqueta', (req, res) => {
         dbservice.getEtiqueta()
-        .then(etiquetas => res.json(etiquetas))
-        .catch(error => res.status(500).send(error));
+            .then(etiquetas => res.json(etiquetas))
+            .catch(error => res.status(500).send(error));
     });
-    
+
     app.get('/Historial_de_Movimiento', (req, res) => {
         dbservice.getHistorialDeMovimiento()
-        .then(historiales => res.json(historiales))
-        .catch(error => res.status(500).send(error));
+            .then(historiales => res.json(historiales))
+            .catch(error => res.status(500).send(error));
     });
-    
+
     app.get('/Tipo_de_Movimiento', (req, res) => {
         dbservice.getTipoDeMovimiento()
-        .then(tipos => res.json(tipos))
-        .catch(error => res.status(500).send(error));
+            .then(tipos => res.json(tipos))
+            .catch(error => res.status(500).send(error));
     });
-    
+
     app.get('/Tipo_de_Cuenta', (req, res) => {
         dbservice.getTipoDeCuenta()
-        .then(cuentas => res.json(cuentas))
-        .catch(error => res.status(500).send(error));
+            .then(cuentas => res.json(cuentas))
+            .catch(error => res.status(500).send(error));
     });
-    
+
     app.get('/Tipo_de_Pago', (req, res) => {
         dbservice.getTipoDePago()
-        .then(pagos => res.json(pagos))
-        .catch(error => res.status(500).send(error));
+            .then(pagos => res.json(pagos))
+            .catch(error => res.status(500).send(error));
     });
-    
+
     app.get('/Estado_de_la_Tarjeta', (req, res) => {
         dbservice.getEstadoDeLaTarjeta()
-        .then(estados => res.json(estados))
-        .catch(error => res.status(500).send(error));
+            .then(estados => res.json(estados))
+            .catch(error => res.status(500).send(error));
     });
-    
+
     app.get('/Participante', (req, res) => {
         dbservice.getParticipante()
-        .then(participantes => res.json(participantes))
-        .catch(error => res.status(500).send(error));
+            .then(participantes => res.json(participantes))
+            .catch(error => res.status(500).send(error));
     });
-    
+
     app.get('/Datos_de_Tarjeta', (req, res) => {
         dbservice.getDatosDeTarjeta()
-        .then(datos => res.json(datos))
-        .catch(error => res.status(500).send(error));
+            .then(datos => res.json(datos))
+            .catch(error => res.status(500).send(error));
     });
+
+    //estos son los post para agregar los datos
+    app.post('/usuarios', (req, res) => {
+        const newUser = req.body;
+        databaseSer
+            .crearUsuario(
+                newUser.Nombre,
+                newUser.Correo_Electronico,
+                newUser.Rol,
+                newUser.Fecha_de_Registro,
+                newUser.Tipo_de_Cuenta,
+                newUser.Tipo_de_Pago
+            )
+            .then(() => {
+                res.json({ message: "Agregado con éxito" });
+            })
+            .catch(e => {
+                res.status(500).send(e);
+            });
+    });
+
+
+    app.post('/tableros', (req, res) => {
+        const newTablero = req.body;
+        databaseSer
+            .crearTablero(
+                newTablero.Nombre_del_Tablero,
+                newTablero.Descripcion,
+                newTablero.Fecha_de_Creacion,
+                newTablero.Usuario_Propietario
+            )
+            .then(() => {
+                res.json({ message: "Agregado con éxito" });
+            })
+            .catch(e => {
+                res.status(500).send(e);
+            });
+    });
+
+
+    app.post('/listas', (req, res) => {
+        const newList = req.body;
+        databaseSer
+            .crearListaDeTarea(
+                newList.Nombre_de_la_Lista,
+                newList.Posicion_en_el_Tablero,
+                newList.Tablero_Perteneciente
+            )
+            .then(() => {
+                res.json({ message: "Agregado con éxito" });
+            })
+            .catch(e => {
+                res.status(500).send(e);
+            });
+    });
+
+
+    app.post('/tarjetas', (req, res) => {
+        const newCard = req.body;
+        databaseSer
+            .crearTarjeta(
+                newCard.Titulo_de_la_Tarjeta,
+                newCard.Descripcion,
+                newCard.Fecha_de_Creacion,
+                newCard.Lista_Perteneciente,
+                newCard.Asignado_a,
+                newCard.Estado_de_la_Tarjeta
+            )
+            .then(() => {
+                res.json({ message: "Agregado con éxito" });
+            })
+            .catch(e => {
+                res.status(500).send(e);
+            });
+    });
+
+
+    app.post('/comentarios', (req, res) => {
+        const newComment = req.body;
+        databaseSer
+            .crearComentario(
+                newComment.Texto_del_Comentario,
+                newComment.Fecha_de_Publicacion,
+                newComment.Usuario_que_Realizo_el_Comentario,
+                newComment.Tarjeta_que_se_Refiere
+            )
+            .then(() => {
+                res.json({ message: "Agregado con éxito" });
+            })
+            .catch(e => {
+                res.status(500).send(e);
+            });
+    });
+
+
+    app.post('/etiquetas', (req, res) => {
+        const newTag = req.body;
+        databaseSer
+            .crearEtiqueta(
+                newTag.Nombre_de_la_Etiqueta,
+                newTag.Color_de_la_Etiqueta
+            )
+            .then(() => {
+                res.json({ message: "Agregado con éxito" });
+            })
+            .catch(e => {
+                res.status(500).send(e);
+            });
+    });
+
+
+    app.post('/historiales', (req, res) => {
+        const newHistory = req.body;
+        databaseSer
+            .crearHistorialDeMovimiento(
+                newHistory.Fecha_y_Hora_del_Movimiento,
+                newHistory.Usuario_que_Realizo_el_Movimiento,
+                newHistory.Tipo_de_Movimiento,
+                newHistory.Tarjeta_Afectada
+            )
+            .then(() => {
+                res.json({ message: "Agregado con éxito" });
+            })
+            .catch(e => {
+                res.status(500).send(e);
+            });
+    });
+
+
+    app.post('/tiposdemovimiento', (req, res) => {
+        const newTipoMovimiento = req.body;
+        databaseSer
+            .crearTipoDeMovimiento(
+                newTipoMovimiento.Nombre_del_Tipo_de_Movimiento,
+                newTipoMovimiento.Descripcion_del_Tipo_de_Movimiento
+            )
+            .then(() => {
+                res.json({ message: "Agregado con éxito" });
+            })
+            .catch(e => {
+                res.status(500).send(e);
+            });
+    });
+
+
+    app.post('/tiposdecuenta', (req, res) => {
+        const newTipoCuenta = req.body;
+        databaseSer
+            .crearTipoDeCuenta(
+                newTipoCuenta.Nombre_del_Tipo_de_Cuenta,
+                newTipoCuenta.Descripcion_del_Tipo_de_Cuenta
+            )
+            .then(() => {
+                res.json({ message: "Agregado con éxito" });
+            })
+            .catch(e => {
+                res.status(500).send(e);
+            });
+    });
+
+
+    app.post('/tiposdepago', (req, res) => {
+        const newTipoPago = req.body;
+        databaseSer
+            .crearTipoDePago(
+                newTipoPago.Nombre_del_Tipo_de_Pago,
+                newTipoPago.Descripcion_del_Tipo_de_Pago
+            )
+            .then(() => {
+                res.json({ message: "Agregado con éxito" });
+            })
+            .catch(e => {
+                res.status(500).send(e);
+            });
+    });
+
+
+    app.post('/estadosdetarjeta', (req, res) => {
+        const newEstadoTarjeta = req.body;
+        databaseSer
+            .crearEstadoDeLaTarjeta(
+                newEstadoTarjeta.Nombre_del_Estado,
+                newEstadoTarjeta.Descripcion_del_Estado
+            )
+            .then(() => {
+                res.json({ message: "Agregado con éxito" });
+            })
+            .catch(e => {
+                res.status(500).send(e);
+            });
+    });
+
+
+    app.post('/participantes', (req, res) => {
+        const newParticipante = req.body;
+        databaseSer
+            .crearParticipante(
+                newParticipante.Usuario_Participante,
+                newParticipante.Tarjeta_asociada
+            )
+            .then(() => {
+                res.json({ message: "Agregado con éxito" });
+            })
+            .catch(e => {
+                res.status(500).send(e);
+            });
+    });
+
+
+    app.post('/datosdetarjeta', (req, res) => {
+        const newDatosTarjeta = req.body;
+        databaseSer
+            .crearDatosDeTarjeta(
+                newDatosTarjeta.Numero_de_Tarjeta,
+                newDatosTarjeta.Mes_de_Vencimiento,
+                newDatosTarjeta.Ano_de_Vencimiento,
+                newDatosTarjeta.Codigo_de_Seguridad,
+                newDatosTarjeta.Tipo_de_Pago
+            )
+            .then(() => {
+                res.json({ message: "Agregado con éxito" });
+            })
+            .catch(e => {
+                res.status(500).send(e);
+            });
+    });
+
+
 };
